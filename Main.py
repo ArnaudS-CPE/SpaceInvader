40fee5bb400e1371986996c144e81578af9ff943
Arnaud Sibenaler <arnaud.sibenaler@free.fr>
2020-12-15 10:10:22 +0100

    ajout vaisseau

--- Main.py
+++ Main.py
@@ -10,12 +10,15 @@ LargeurCanevas = 480
 HauteurCanevas = 320
 X=50
 Y=50
+x=240
+y=300
+
 DX=3
 
 
-def deplacement () :
+def deplacementAlien () :
     global X, Y, DX
-    if X-12+DX > LargeurCanevas :
+    if X+12+DX > LargeurCanevas :
         X = 2*(LargeurCanevas-12)-X
         DX = -DX        
     if X-12+DX < 0:
@@ -23,7 +26,18 @@ def deplacement () :
         DX = -DX
     X=X+DX
     Canevas.coords(Alien, X-12, Y-12, X+12, Y+12)
-    Mafenetre.after(20,deplacement)
+    Mafenetre.after(20,deplacementAlien)
+
+def deplacementVaisseau (event) :
+    global x
+    touche = event.keysym
+    print(touche)
+    if touche == 'Right' :
+        x += 5
+    if touche == 'Left' :
+        x-= 5
+    Canevas.coords(Vaisseau, x-15, y-8, x+15, y+8)
+
 
 # création de la fenêtre principale
 Mafenetre = Tk()
@@ -33,10 +47,15 @@ Mafenetre.title("Space Invaders")
 Canevas = Canvas(Mafenetre, height=HauteurCanevas, width=LargeurCanevas, bg='navy')
 Canevas.pack(padx=5, pady=5)
 
-# création d'un objet
+# création d'un alien
 Alien = Canevas.create_oval(X-12, Y-12, X+12, Y+12, width=3, outline='green', fill='yellow')
 
+# création d'un vaisseau
+Vaisseau = Canevas.create_rectangle(x-15, y-8, x+15, y+8, width=2, outline='red', fill='white')
 
-deplacement()
+Canevas.focus_set()
+Canevas.bind('<Key>',deplacementVaisseau)
+
+deplacementAlien()
 
 Mafenetre.mainloop()
