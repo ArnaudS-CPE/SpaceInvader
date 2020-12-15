d2b6ade5cd610482803b31dbc84a178e8b172f3a
Alexandre Burlot/usr/local/bin/python3 <burlotalexandre@yahoo.fr>
2020-12-15 12:10:59 +0100

    TODO complet

--- maLib.py
+++ maLib.py
@@ -6,7 +6,22 @@
 # permettre à l'alien de descendre petit à petit lorsqu'il à fait un allé retour
 # Si l'allien arrive au niveau du vaisseau et le touche, le vaiseau est détruit et la partie est terminée
 # mettre maintenant plusieur aliens sur la meme ligne et verifier quetout se passe bien
-# 
+# permettre aux aliens de tirer vers le bas, le déclenchement est aléatoire, même vitesse que ceux du vaiseau
+# si un tir alien atteint le vaiseau, les deux sont détruits et la partie est finie
+# faire 3 vies pour le joueur et lui afficher
+# créer des ilots pour que le vaisseau puisse se cacher, se détruisent petità petit si ils subissent un tir alien
+# Faire apparaitre un ennemi bonus et gerer son état
+# introduire les scores transformer les aliens soit en formes simples soit en images
+# barre du menu
+# BONUS :
+# • Augmenter la vitesse des Aliens lorsqu’ils sont moins nombreux.
+# • Gérer le passage de la soucoupe
+# • Créer plusieurs niveaux à votre jeu
+# • Ajouter une image de fond (différente pour chaque niveau)
+# • Gestion des meilleurs scores (avec inscription dans un fichier texte)
+# • Permettre à l’utilisateur de changer les touches de contrôle
+# • Mettre des cheat codes afin de gagner des vies supplémentaires
+# • Laisser parler votre imagination et n’hésitez pas à demander conseil à vos ainés qui auraient perdu un temps précieux à jouer à ce jeu ! 
 
 from tkinter import *
 import time 
