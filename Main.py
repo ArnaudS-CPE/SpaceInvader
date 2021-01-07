cbd4b0fd0f6b831eb4f580ced61e2099515a9d65
Arnaud Sibenaler <arnaud.sibenaler@free.fr>
2021-01-07 02:19:53 +0100

    todo

--- Main.py
+++ Main.py
@@ -1,14 +1,8 @@
 # BURLOT Alexandre, SIBENALER Arnaud
 # début du projet le 15/12/2020
-# Objectif : réalisation d'un space invader sous Tkinter
+# Objectif : réalisation d'un space invaders sous Tkinter
 # TODO : lien du sujet du tp : https://prod.e-campus.cpe.fr/pluginfile.php/53617/mod_resource/content/1/TP3.pdf
-# Gerer la collision entre le tir et l'Alien, ils doivent tous les deux disparaitres
-# Si l'allien arrive au niveau du vaisseau et le touche, le vaiseau est détruit et la partie est terminée
-# mettre maintenant plusieur aliens sur la meme ligne et verifier quetout se passe bien
-# permettre aux aliens de tirer vers le bas, le déclenchement est aléatoire, même vitesse que ceux du vaiseau
-# si un tir alien atteint le vaiseau, les deux sont détruits et la partie est finie
-# faire 3 vies pour le joueur et lui afficher
-# créer des ilots pour que le vaisseau puisse se cacher, se détruisent petit à petit si ils subissent un tir alien
+# afficher les vies du vaisseau
 # Faire apparaitre un ennemi bonus et gerer son état
 # introduire les scores transformer les aliens soit en formes simples soit en images
 # barre du menu
@@ -21,6 +15,7 @@
 # • Permettre à l’utilisateur de changer les touches de contrôle
 # • Mettre des cheat codes afin de gagner des vies supplémentaires
 # • Laisser parler votre imagination et n’hésitez pas à demander conseil à vos ainés qui auraient perdu un temps précieux à jouer à ce jeu ! 
+
 
 from tkinter import Tk, Button, Canvas, messagebox
 import maLib as mL
