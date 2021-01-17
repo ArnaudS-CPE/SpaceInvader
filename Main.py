48fee671baab701474a419eb1e5fce1be9d09f8a
Alexandre Burlot/usr/local/bin/python3 <burlotalexandre@yahoo.fr>
2021-01-17 11:50:42 +0100

    ecran accueil

--- Main.py
+++ Main.py
@@ -31,6 +31,7 @@ viesVaisseau = 3 # vies du vaisseau
 def jeu(scoreactuel, niveau):
     global frequence, viesVaisseau
 
+    
     # création de la fenetre
     mw = Tk()
     mw.geometry(str(mL.LargeurCanevas+100) + "x" + str(mL.HauteurCanevas))
@@ -39,7 +40,10 @@ def jeu(scoreactuel, niveau):
 
     # création des widgets
     canevas = Canvas(mw, width = mL.LargeurCanevas, height = mL.HauteurCanevas, bg = 'grey')
-    backgroundPicture = PhotoImage(file = 'Earth.gif')
+    if niveau == 0:
+        backgroundPicture = PhotoImage(file = 'accueil_space_invader.gif')
+    else:
+        backgroundPicture = PhotoImage(file = 'Earth.gif')
     canevas.create_image(453,353, image=backgroundPicture)
     
     quit = Button(mw, text = "Quit", command = mw.destroy)
@@ -56,67 +60,70 @@ def jeu(scoreactuel, niveau):
     newGameButton.grid(row=2, column=3, sticky='N', padx=5)
     canevas.grid(row=1, column=0, rowspan=3, columnspan=3, padx=5)
 
-    # instanciation de l'objet vaisseau
-    vaisseau = mL.Vaisseau(10, 600, viesVaisseau, canevas, mw)
-    vaisseau.setScore(scoreactuel) # lui donne le score atteint jusque la, vaut 0 si le niveau est à 1
-    
-    # met le focus sur le canvas pour les actions à effectuer
-    canevas.focus_set()
-    canevas.bind('<Key>',vaisseau.evenement)
-
-    if niveau%3 != 0: # tous les 3 lvl, c'est l'alien bonus qui apparaît
-        # crée la ligne 1 d'aliens 
-        alien1 = mL.Alien(10, 10, 50, 50, frequence, vaisseau, canevas, mw)
-        alien2 = mL.Alien(110, 10, 50, 50, frequence, vaisseau, canevas, mw)
-        alien3 = mL.Alien(210, 10, 50, 50, frequence, vaisseau, canevas, mw)
-        alien4 = mL.Alien(310, 10, 50, 50, frequence, vaisseau, canevas, mw)
-        alien5 = mL.Alien(410, 10, 50, 50, frequence, vaisseau, canevas, mw)
-        alien6 = mL.Alien(510, 10, 50, 50, frequence, vaisseau, canevas, mw)
-        alien7 = mL.Alien(610, 10, 50, 50, frequence, vaisseau, canevas, mw)
-
-        # crée la ligne 2 d'aliens 
-        alien8 = mL.Alien(10, 100, 50, 50, frequence, vaisseau, canevas, mw)
-        alien9 = mL.Alien(110, 100, 50, 50, frequence, vaisseau, canevas, mw)
-        alien10 = mL.Alien(210, 100, 50, 50, frequence, vaisseau, canevas, mw)
-        alien11 = mL.Alien(310, 100, 50, 50, frequence, vaisseau, canevas, mw)
-        alien12 = mL.Alien(410, 100, 50, 50, frequence, vaisseau, canevas, mw)
-        alien13 = mL.Alien(510, 100, 50, 50, frequence, vaisseau, canevas, mw)
-        alien14 = mL.Alien(610, 100, 50, 50, frequence, vaisseau, canevas, mw)
-
-        # crée la ligne 3 d'aliens 
-        alien15 = mL.Alien(10, 190, 50, 50, frequence, vaisseau, canevas, mw)
-        alien16 = mL.Alien(110, 190, 50, 50, frequence, vaisseau, canevas, mw)
-        alien17 = mL.Alien(210, 190, 50, 50, frequence, vaisseau, canevas, mw)
-        alien18 = mL.Alien(310, 190, 50, 50, frequence, vaisseau, canevas, mw)
-        alien19 = mL.Alien(410, 190, 50, 50, frequence, vaisseau, canevas, mw)
-        alien20 = mL.Alien(510, 190, 50, 50, frequence, vaisseau, canevas, mw)
-        alien21 = mL.Alien(610, 190, 50, 50, frequence, vaisseau, canevas, mw)
-
-        alien1.createurTir() # initie le tir des aliens
-
+    if niveau == 0:
+        fff
     else:
-        alienbonus = mL.AlienBonus(100, 100, 200, 80, vaisseau, canevas, mw) # instanciation alienbonus
-        alienbonus.createurTirBonus() # initie le tir de l'alien
+        # instanciation de l'objet vaisseau
+        vaisseau = mL.Vaisseau(10, 600, viesVaisseau, canevas, mw)
+        vaisseau.setScore(scoreactuel) # lui donne le score atteint jusque la, vaut 0 si le niveau est à 1
+        
+        # met le focus sur le canvas pour les actions à effectuer
+        canevas.focus_set()
+        canevas.bind('<Key>',vaisseau.evenement)
 
-    # lance les actualisaions en temps réel des informations du joueur
-    checkWinning(niveau,vaisseau, mw)
-    checkScore(score,vaisseau, mw)
-    checkVies(vies,vaisseau,mw)
+        if niveau%3 != 0: # tous les 3 lvl, c'est l'alien bonus qui apparaît
+            # crée la ligne 1 d'aliens 
+            alien1 = mL.Alien(10, 10, 50, 50, frequence, vaisseau, canevas, mw)
+            alien2 = mL.Alien(110, 10, 50, 50, frequence, vaisseau, canevas, mw)
+            alien3 = mL.Alien(210, 10, 50, 50, frequence, vaisseau, canevas, mw)
+            alien4 = mL.Alien(310, 10, 50, 50, frequence, vaisseau, canevas, mw)
+            alien5 = mL.Alien(410, 10, 50, 50, frequence, vaisseau, canevas, mw)
+            alien6 = mL.Alien(510, 10, 50, 50, frequence, vaisseau, canevas, mw)
+            alien7 = mL.Alien(610, 10, 50, 50, frequence, vaisseau, canevas, mw)
 
-    # blocs de murs
-    for k in range(8) :
-        for l in range(2) :
-            mur1 = mL.Mur(20, 20, 75+k*20, 520+l*20, canevas, mw)
+            # crée la ligne 2 d'aliens 
+            alien8 = mL.Alien(10, 100, 50, 50, frequence, vaisseau, canevas, mw)
+            alien9 = mL.Alien(110, 100, 50, 50, frequence, vaisseau, canevas, mw)
+            alien10 = mL.Alien(210, 100, 50, 50, frequence, vaisseau, canevas, mw)
+            alien11 = mL.Alien(310, 100, 50, 50, frequence, vaisseau, canevas, mw)
+            alien12 = mL.Alien(410, 100, 50, 50, frequence, vaisseau, canevas, mw)
+            alien13 = mL.Alien(510, 100, 50, 50, frequence, vaisseau, canevas, mw)
+            alien14 = mL.Alien(610, 100, 50, 50, frequence, vaisseau, canevas, mw)
 
-    for k in range(8) :
-        for l in range(2) :
-            mur2 = mL.Mur(20, 20, 375+k*20, 520+l*20, canevas, mw)
+            # crée la ligne 3 d'aliens 
+            alien15 = mL.Alien(10, 190, 50, 50, frequence, vaisseau, canevas, mw)
+            alien16 = mL.Alien(110, 190, 50, 50, frequence, vaisseau, canevas, mw)
+            alien17 = mL.Alien(210, 190, 50, 50, frequence, vaisseau, canevas, mw)
+            alien18 = mL.Alien(310, 190, 50, 50, frequence, vaisseau, canevas, mw)
+            alien19 = mL.Alien(410, 190, 50, 50, frequence, vaisseau, canevas, mw)
+            alien20 = mL.Alien(510, 190, 50, 50, frequence, vaisseau, canevas, mw)
+            alien21 = mL.Alien(610, 190, 50, 50, frequence, vaisseau, canevas, mw)
 
-    for k in range(8) :
-        for l in range(2) :
-            mur3 = mL.Mur(20, 20, 665+k*20, 520+l*20, canevas, mw)
+            alien1.createurTir() # initie le tir des aliens
 
-    mw.mainloop() # boucle principale de la fenêtre Tkinter
+        else:
+            alienbonus = mL.AlienBonus(100, 100, 200, 80, vaisseau, canevas, mw) # instanciation alienbonus
+            alienbonus.createurTirBonus() # initie le tir de l'alien
+
+        # lance les actualisaions en temps réel des informations du joueur
+        checkWinning(niveau,vaisseau, mw)
+        checkScore(score,vaisseau, mw)
+        checkVies(vies,vaisseau,mw)
+
+        # blocs de murs
+        for k in range(8) :
+            for l in range(2) :
+                mur1 = mL.Mur(20, 20, 75+k*20, 520+l*20, canevas, mw)
+
+        for k in range(8) :
+            for l in range(2) :
+                mur2 = mL.Mur(20, 20, 375+k*20, 520+l*20, canevas, mw)
+
+        for k in range(8) :
+            for l in range(2) :
+                mur3 = mL.Mur(20, 20, 665+k*20, 520+l*20, canevas, mw)
+
+        mw.mainloop() # boucle principale de la fenêtre Tkinter
 
 
     
